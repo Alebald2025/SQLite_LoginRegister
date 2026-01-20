53fc885a6465078d812dd489b25a7ae4e7035901
Alejandro Baldeon E <a24josbalesp@inspedralbes.cat>
2026-01-20 18:03:35 +0100

    Cambios visuales

---
+++ Assets/Scripts/PanelAnimation.cs
@@ -0,0 +1,24 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelAnimation : MonoBehaviour
+{
+    [SerializeField] private RectTransform panelRect; 
+    [SerializeField] private float duracion = 0.3f;
+
+    public void Expandir()
+    {
+        Vector2 tamañoFinal = new Vector2(270f, 350f); 
+        panelRect.DOSizeDelta(tamañoFinal, duracion)
+                 .SetEase(Ease.OutQuad); // Easing suave (opcional: InOutQuad, etc.)
+    }
+
+    public void Contraer()
+    {
+        Vector2 tamañoOriginal = new Vector2(270f, 260f);
+        panelRect.DOSizeDelta(tamañoOriginal, duracion)
+                 .SetEase(Ease.InQuad);
+    }
+}
